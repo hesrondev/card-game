8c73bfb5f80871fb0e08cac90dde77fae6b84327
mimi6060 <michellammens@MacBook-Pro-de-michel.local>
2017-04-13 12:12:07 +0200

    init

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var express    = require('express');
+var path       = require('path');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index      = require('./routes/index');
+
+
+var port = 3000 ;
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// view engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// set static folder
+
+app.use(express.static(path.join(__dirname,'client')));
+
+// body paser MV
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/',index);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+app.listen(port,function(){
+    console.log('serveur starterd on port'+port);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
