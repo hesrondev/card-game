0e70a37391a6e90f3e33cf6a189b379ded941929
Louange BIZI <l.bizi@gop.link>
2017-04-13 13:50:16 +0200

    init project

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ergoCardGameApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ergoCardGameApp
+ */
+angular.module('ergoCardGameApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
