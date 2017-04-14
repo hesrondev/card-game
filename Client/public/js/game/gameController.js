0a549f5975440d2280a3de1e8b760d622a7c1045
Louange BIZI <l.bizi@gop.link>
2017-04-14 14:54:10 +0200

    update

--- Client/public/js/game/gameController.js
+++ Client/public/js/game/gameController.js
@@ -1,17 +1,26 @@
+app.controller('GameController', function ($scope, GameService) {
 
-app.controller( 'gameController', [ 'Messages' , '$scope', 'gameService', function( Messages, $scope,gameService ) {
 
-gameService.conexionSocket();
-    // Dummy players
 
-    var game = GameService.initGame();
+    GameService.on('game', function (data) {
 
-    $scope.player1 = game.players[0];
-    $scope.player2 = game.players[1];
-    $scope.player3 = game.players[2];
-    $scope.player4 = game.players[3];
+        console.log('Received');
 
-    $scope.centerCards = game.centerCards;
+        var game = data.game;
+
+        $scope.player1 = game.players[0];
+        $scope.player2 = game.players[1];
+        $scope.player3 = game.players[2];
+        $scope.player4 = game.players[3];
+
+        $scope.centerCards = game.centerCards;
+    });
+
+    $scope.sendPseudo = function (pseudo) {
+        GameService.emit('pseudo', pseudo);
+    };
+
+
 
 
     function update() {
@@ -36,3 +45,4 @@ gameService.conexionSocket();
             return root + card.img;
         }
     }
+})
--- app.js
+++ app.js
@@ -1,55 +1,57 @@
-var express     = require('express'),
-     path       = require('path'),
-     bodyParser = require('body-parser'),
-     app        = express(),
-     server     = require('http').createServer(app),
-     io = require('socket.io')(server)
-     index      = require('./routes/index'),
-     game       = require('./utils/game');
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io')(server),
+    index = require('./routes/index'),
+    game = require('./utils/game');
 
 // view engine
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
-app.engine('html',require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 // set static folder
 
-app.use(express.static(path.join(__dirname,'client')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // body paser MV
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/',index);
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use('/', index);
 
 
 
 
-var port = 3000 ;
+var port = 3000;
 
 game.init();
 
 console.log(game.game());
 io.sockets.on('connection', function (socket) {
 
-    socket.on('pseudo', function(pseudo) {
+    socket.on('pseudo', function (pseudo) {
         socket.pseudo = pseudo;
-        console.log(pseudo+' connecter');
+        console.log(pseudo + ' connecter');
         game.addPlayer(pseudo);
-        socket.broadcast.emit('game',game.game());
+        socket.broadcast.emit('game', game.game());
 
     });
 
-  
+
     socket.on('message', function (message) {
         // On récupère le pseudo de celui qui a cliqué dans les variables de session
         console.log(socket.pseudo + ' me parle ! Il me dit : ' + message);
-    }); 
+    });
 
 
 
 });
 
-server.listen(port,function(){
-    console.log('serveur starterd on port'+port);
+server.listen(port, function () {
+    console.log('serveur starterd on port' + port);
 });
