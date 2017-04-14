5b875dba1e5eacbc6e694db2fb6f759af0245543
Louange BIZI <l.bizi@gop.link>
2017-04-14 12:27:46 +0200

    display cards

--- Client/public/js/game/gameController.js
+++ Client/public/js/game/gameController.js
@@ -1,4 +1,34 @@
-app.controller('GameController', function ($scope) {
+app.controller('GameController', function ($scope, GameService) {
 
+
+    // Dummy players
+
+    var game = GameService.initGame();
+
+    $scope.player1 = game.players[0];
+    $scope.player2 = game.players[1];
+    $scope.player3 = game.players[2];
+    $scope.player4 = game.players[3];
+
+    $scope.centerCards = game.centerCards;
+
+
+    function update() {
+
+    }
+
+
+    // Get card img
+
+    $scope.getCardImg = function (card) {
+
+        var root = 'public/img/';
+
+        if (card.state) {
+            return root + card.img;
+        } else {
+            return root + card.img;
+        }
+    }
 
 });
